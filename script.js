4ce31b52c9d49d186703c3bfe56ad79f7594b439
Supratik0b <160757751+Supratik0b@users.noreply.github.com>
2024-10-08 20:16:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = 'bd5e378503939ddaee76f12ad7a97608';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const weatherInfoElement = document.querySelector('.weather-info');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+
+            // Add show class to trigger animation
+            weatherInfoElement.classList.add('show');
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
